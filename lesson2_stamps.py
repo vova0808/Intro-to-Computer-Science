11d19ded98d607241bfbfc09ffbba7c761a1f618
Volodymyr <vova0808@gmail.com>
2016-07-21 19:42:20 +0300

    Update lesson2_stamps.py

--- lesson2_stamps.py
+++ lesson2_stamps.py
@@ -1,3 +1,15 @@
+# Define a procedure, stamps, which takes as its input a positive integer in
+# pence and returns the number of 5p, 2p and 1p stamps (p is pence) required 
+# to make up that value. The return value should be a tuple of three numbers 
+# (that is, your return statement should be followed by the number of 5p,
+# the number of 2p, and the nuber of 1p stamps).
+#
+# Your answer should use as few total stamps as possible by first using as 
+# many 5p stamps as possible, then 2 pence stamps and finally 1p stamps as 
+# needed to make up the total.
+
+
+
 def stamps(pence):
     
     remainder = 0
